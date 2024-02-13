6834b6a820805ba2089573dc89be9586953e1090
Grace Peven <159390652+springmother@users.noreply.github.com>
2024-02-12 16:51:16 -0800

    Update density_graphs.R
    added distance to seed source graph

--- density_graphs.R
+++ density_graphs.R
@@ -61,4 +61,47 @@ PICO_dens =
 
 PICO_PSME = grid.arrange(arrangeGrob(PSME_dens, PICO_dens,  ncol = 2, bottom = textGrob("Distance to Spring (m)", gp = gpar(fontfamily = "serif",fontsize = 11))))
 
+##################################################
+#### distance to seed source by species graph ####
+##################################################
+tree_count_gen <- 
+  tree_counts %>%
+  group_by(Transect_count, Spring_Name, Distance_range_m, Species) %>%
+  summarize(Sum_Density = sum(Density)
+
+# AVG_DENSITY column is the total density at each distance bin for the entire 
+#spring divided by the number of transects per spring
+
+tree_count_gen$Avg_Density = tree_count_gen$Sum_Density/tree_count_gen$Transect_count
+tree_count_gen$Spring_Name = factor(tree_count_gen$Spring_Name)
+tree_count_gen$Distance_range_m <- factor(tree_count_gen$Distance_range_m)
+
+### Adding distance to live tree by spring
+tree_count_springs = transectdata %>%
+  group_by(Spring_Name) %>%
+  summarise(Mean_dist_tree = mean (Average_dist_to_live_tree))
+
+tree_count_gen <- merge(tree_count_gen, tree_count_springs, by = "Spring_Name")
+tree_count_gen$Mean_dist_tree[is.na(tree_count_gen$Mean_dist_tree)] <- 500
+
+tree_count_gen %>% 
+  filter(Species == "PSME" | Species == "PICO")%>%
+  ggplot(aes(Mean_dist_tree, Avg_Density, color = Species))+
+  geom_point(size = 1)+
+  geom_smooth(method = "loess", alpha = 0.1, se = TRUE)+
+  scale_color_manual(values = c("PSME" = "darkgreen", "PICO" = "orange"),labels = c("PSME" = "Douglas Fir", "PICO" = "Lodgepole Pine"))+
+  ggtitle("")+
+  xlab("Distance to suriving seed source (m)")+
+  ylab("Average density per spring (trees/hectare)")+
+  theme_classic()+
+  theme(axis.text.x = element_text(size = 10, family ="serif"), 
+        axis.text.y = element_text(size = 10, family ="serif"), 
+        axis.title.y = element_text(size = 11, family ="serif"),
+        axis.title.x = element_text(size = 11, family ="serif"), 
+        plot.title = element_text(size = 11, family ="serif"),
+        legend.title = element_text(size = 11, family ="serif"), 
+        legend.text = element_text(size = 10, family ="serif"),
+        legend.position = "bottom",  
+        legend.box = "horizontal"
+  )
 
